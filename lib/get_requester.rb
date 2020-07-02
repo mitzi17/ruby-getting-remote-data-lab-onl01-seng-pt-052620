f1b1e13491a6b16b77ea5e943754cf61f700795d
Mitzi Estrada <m_estrada17@live.com>
2020-07-02 05:20:21 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -9,7 +9,7 @@ class GetRequester
   
   def get_response_body
     uri = URI.parse(url)
-    response = NET::HTTP.get_response(url)
+    response = NET::HTTP.get_response(uri)
     response.body
   end
   
